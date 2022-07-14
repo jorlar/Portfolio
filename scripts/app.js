7144bcd35c5c7880583ef40c6ad404ad28e58a6c
jorlar <jqern.larsen@gmail.com>
2022-07-15 00:34:55 +0200

    :art: Add JS and styling

---
+++ scripts/app.js
@@ -0,0 +1,19 @@
+// Declare consts
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controlls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
+
+//Declare functions 
+function PageTransitions() {
+    //Btn click active class
+    for(let i = 0; i < sectBtn.length; i++) {
+        sectBtn[i].addEventListener('click', function() {
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' active-btn'
+        })
+    }
+}
+
+PageTransitions();
